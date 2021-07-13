78fa04f10c31519bb1b8eaf24dae3a4f3e2a32de
Morgan Segura <morgan@jointhebibleproject.com>
2021-07-13 13:43:22 -0700

    Creates user model and graphql mutation

--- server/models/user.js
+++ server/models/user.js
@@ -6,7 +6,8 @@ const UserSchema = new mongoose.Schema(
 		username: {
 			type: String,
 			required: true,
-			index: true
+			index: true,
+			unique: true
 		},
 		name: {
 			type: String
@@ -14,13 +15,14 @@ const UserSchema = new mongoose.Schema(
 		email: {
 			type: String,
 			required: true,
-			index: true
+			index: true,
+			unique: true
 		},
 		images: {
 			type: Array,
 			default: {
 				url: 'https://via.placeholder.com/200x200.png?text=Profile',
-				public_id: '123'
+				public_id: Date.now
 			}
 		},
 		about: {
